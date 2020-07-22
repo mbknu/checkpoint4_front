a5854e9f57a999b45c529d37f61cb6c9266f8f5d
mbknu <bouchelot.m@gmail.com>
2020-07-22 13:13:58 +0200

    fix app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./components/Intro/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import HomePage from "./components/HomePage/HomePage";
+import Login from "./components/auth/Login";
+import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
+import Projects from "./components/Projects/Projects";
+import Misc from "./components/Misc/Misc";
+import Facts from "./components/Misc/Facts/Facts";
+import Battle from "./components/Battle/Battle";
+import About from "./components/About/About";
+import Profile from "./components/auth/Profile";
+import ChuckJokes from "./components/Misc/Chuck/ChuckJokes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Route path="/" component={Home} exact />
+        <Switch>
+          <Route path="/homepage" component={HomePage} />
+          <Route path="/login" component={Login} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/misc" component={Misc} />
+          <Route path="/facts" component={Facts} />
+          <Route path="/battle" component={Battle} />
+          <Route path="/about" component={About} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/chuck" component={ChuckJokes} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
