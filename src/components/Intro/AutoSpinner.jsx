656b61b5594fc59ac43aabd10b81f7e8e7ee17e4
mbknu <bouchelot.m@gmail.com>
2020-07-22 13:10:06 +0200

    add misc and intro

---
+++ src/components/Intro/AutoSpinner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { AtomSpinner } from "react-epic-spinners";
+
+function AutoSpinner() {
+  return (
+    <div className="auto-spinner">
+      <AtomSpinner color="#ff6961" size="250"></AtomSpinner>
+    </div>
+  );
+}
+
+export default AutoSpinner;
